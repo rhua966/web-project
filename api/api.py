7052d0fa6811806aa8b872d0a1377eeefc4a660a
Rui Huang <rhua966@Kyles-MBP.fritz.box>
2020-09-19 13:51:51 +1200

    Basic Todo App

--- api/api.py
+++ api/api.py
@@ -1,8 +1,15 @@
 import time
-from .venv/lib/flask import Flask
+from flask import Flask
 
 app = Flask(__name__)
+app.run(debug=True)
+
+
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+@app.route('/hello/<username>')
+def hello(username):
+    return f"Hello {username}, thanks for using Flask!"
