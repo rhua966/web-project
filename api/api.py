051b7b5f9985b1abfeda6822dbc307b296725fd6
Rui Huang <rhua966@Kyles-MBP.fritz.box>
2020-09-21 19:17:57 +1200

    Set up database using SQLAlchemy

--- api/api.py
+++ api/api.py
@@ -1,16 +1,32 @@
 import time
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] - 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class VideoModel(db.Model):
+    # Define all the fields of the database
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Video(name = {name}, views = {views}, likes = {likes}"
+
+db.create_all() # Only uncomment this line on the first you run it, otherwise data will be replaced everytime server runs 
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
 video_put_args.add_argument("views", type=int, help="Views of the video is required")
 video_put_args.add_argument("likes", type=int, help="Likes of the video is required")
-videos = {}
+# videos = {}
 
 def abort_if_video_doesnt_exist(video_id):
     if video_id not in videos:
